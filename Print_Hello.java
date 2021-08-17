c436d3fed3cf28e5c91ffc6cf7c6d82a86d9635a
Patel Aditi <77919745+aditipatelap@users.noreply.github.com>
2021-08-17 11:31:21 +0530

    Create Print_Hello.java

---
+++ Print_Hello.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class hello{
+
+    public static void main(String[]args){
+    
+        System.out.print("Hello");
+        System.out.print("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.print("Hello\n");
+        System.out.print("1");
+        System.out.println(1);
+
+    }
+}
