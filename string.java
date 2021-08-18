ac39f3acf6db30cf2accc2031a89bc5a2ae21eab
Patel Aditi <77919745+aditipatelap@users.noreply.github.com>
2021-08-18 12:23:47 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class string{
+
+    public static void main(String[]args){
+    String s1 = "HeLlO";
+    String s2 = "JavA";
+    System.out.println(s1);
+    System.out.println(s2);
+    System.out.println(s1+s2);
+    s1 = s1.toUpperCase();
+    s2 = s2.toLowerCase();
+    System.out.println(s1);
+    System.out.println(s2);
+    System.out.println(s1 + " " + s2);
+
+    }
+}
