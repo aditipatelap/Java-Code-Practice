897581a030b0e4bcb9c08db934d681037d1df62f
Patel Aditi <77919745+aditipatelap@users.noreply.github.com>
2021-08-18 11:29:21 +0530

    Create input_int.java

---
+++ input_int.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class input_int{
+
+    public static void main(String[]args){
+
+    Scanner scn = new Scanner (System.in);
+    int n = scn.nextInt();
+    System.out.println("The value of n is "+ n);
+    }
+}
