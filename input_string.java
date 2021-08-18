3abeadcd1caa14876b86d78daca5c75b957cb303
Patel Aditi <77919745+aditipatelap@users.noreply.github.com>
2021-08-18 11:30:03 +0530

    Create input_string.java

---
+++ input_string.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class input_string{
+
+    public static void main(String[]args){
+    
+    Scanner abc = new Scanner (System.in);
+    String name = abc.nextLine();
+    System.out.println("Your name is "+name);
+    }
+}
