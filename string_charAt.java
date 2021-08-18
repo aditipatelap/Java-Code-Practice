c57a3346aaada5bd25a8b73441a2839225cc141a
Patel Aditi <77919745+aditipatelap@users.noreply.github.com>
2021-08-18 11:42:24 +0530

    Create string_charAt.java

---
+++ string_charAt.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class string_charAt{
+
+    public static void main(String[]args){
+    //System scan = new Scanner(System.in);
+    String s = "Hello Java";
+    //s.charAt(0) = 'Z';  <-- It's not right.
+    System.out.println(s.charAt(0));
+    System.out.println(s.charAt(1));
+    System.out.println(s.charAt(2));
+    System.out.println(s.charAt(5));
+    System.out.println(s.charAt(8));
+
+    }
+}
