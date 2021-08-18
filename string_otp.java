141789f62612cd0098daa9dd64f3cfd2304e92ea
Patel Aditi <77919745+aditipatelap@users.noreply.github.com>
2021-08-18 11:38:11 +0530

    Create string_otp.java

---
+++ string_otp.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class string_otp{
+  
+    public static void main(String[]args)
+    {
+      Scanner scan = new Scanner(System.in);
+      String s1 = "Hello Java";
+      System.out.println(s1);
+    }
+}
