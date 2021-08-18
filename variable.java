888ce629d2939c2aa2694fa6a88897bff983f07a
Patel Aditi <77919745+aditipatelap@users.noreply.github.com>
2021-08-18 11:28:35 +0530

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class variable{
+
+    public static void main(String[]args){
+        int x = (int)Math.pow(2,3);
+        System.out.println(x);
+        System.out.println(Math.pow(2,2));
+        int a = 15;
+        int b = 30;
+        int sum = a + b;
+        System.out.println("The sum of "+ a + " and " + b + " is " + sum);
+
+
+    }
+}
